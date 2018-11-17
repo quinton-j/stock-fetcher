a90af06f615a953c2680cfe47bbdf4dc397405a3
quinton <justin.quinton@gmail.com>
2018-11-17 13:31:45 -0500

    Sorted symbols

--- yahoo-stock-scrapper.js
+++ yahoo-stock-scrapper.js
@@ -10,7 +10,7 @@ module.exports = {
         if (!stocks || !stocks.length) {
             return [];
         } else {
-            return Promise.all(stocks.map(getStock));
+            return Promise.all(stocks.sort().map(getStock));
         }
     },
 }
