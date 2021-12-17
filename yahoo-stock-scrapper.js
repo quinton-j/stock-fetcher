c0a37b5b92b8632bbd2aea4757bcae77dee0675e
Q <justin.quinton@gmail.com>
2021-12-16 21:48:35 -0500

    Fix yahoo scrape for data change

--- yahoo-stock-scrapper.js
+++ yahoo-stock-scrapper.js
@@ -31,20 +31,20 @@ function parseStockJson(stock, body) {
     const splitSymbol = stock.split('.');
     const data = /^root\.App\.main = (.*);$/gm.exec(body);
     const json = JSON.parse(data[1]);
-    const stockData = json.context.dispatcher.stores.QuoteSummaryStore;
-    const summaryDetail = stockData.summaryDetail;
+    const quoteSummaryStore = json.context.dispatcher.stores.QuoteSummaryStore;
+    const streamDataStore = json.context.dispatcher.stores.StreamDataStore.quoteData[stock];
+    const summaryDetail = quoteSummaryStore.summaryDetail;
 
-    const current = stockData.price.regularMarketPrice.raw;
-    const stockYield = summaryDetail.yield && summaryDetail.yield.raw;
+    const current = quoteSummaryStore.price.regularMarketPrice.raw;
 
     return {
         symbol: splitSymbol[0],
         exchange: splitSymbol[1],
         close: current,
-        high52: summaryDetail.fiftyTwoWeekHigh && summaryDetail.fiftyTwoWeekHigh.raw,
-        low52: summaryDetail.fiftyTwoWeekLow &&  summaryDetail.fiftyTwoWeekLow.raw,
-        div: stockYield ? stockYield * current : (summaryDetail.dividendRate && summaryDetail.dividendRate.raw),
-        yield: stockYield ? stockYield : (summaryDetail.dividendYield && summaryDetail.dividendYield.raw),
-        peRatio: !summaryDetail.trailingPE ? null : summaryDetail.trailingPE.raw,
+        high52: streamDataStore.fiftyTwoWeekHigh.raw,
+        low52: streamDataStore.fiftyTwoWeekLow.raw,
+        div: summaryDetail.dividendRate && summaryDetail.dividendRate.raw,
+        yield: summaryDetail.dividendYield && summaryDetail.dividendYield.raw,
+        peRatio: summaryDetail.trailingPE && summaryDetail.trailingPE.raw,
     };
 }
